3f23986649c785208c73d6e7bb2d00b3233c2d83
Alexus Chanthadara <alexus.n.chanthadara@gmail.com>
2026-04-13 11:02:31 -0400

    updating SQL records

--- week-02/Ex1B_first_query.sql
+++ week-02/Ex1B_first_query.sql
@@ -1,3 +1,7 @@
 /*
 Sakila is a database used to collect information about movies and and crew members for use by investors.
 */
+
+USE sakila;
+SELECT * FROM actor; -- Retrieved [200] records
+SELECT *  FROM film; -- Retrieved [1000] records
