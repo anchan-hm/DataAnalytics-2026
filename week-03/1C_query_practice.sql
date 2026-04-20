cca86155c43bf0a562b16d019b015338587c3213
Alexus Chanthadara <alexus.n.chanthadara@gmail.com>
2026-04-20 09:20:49 -0400

    updating 1C

--- week-03/1C_query_practice.sql
+++ week-03/1C_query_practice.sql
@@ -16,5 +16,15 @@ WHERE UnitsInStock >= 100
 ORDER BY UnitPrice DESC;
 
 -- 3) What are the products that we carry where we have at least 100 units on hand?
+-- Sirop d'rable, Grandma's Boysenberry Spread, Pt chinois, Gustaf's Knckebrd, Inlagd Sill,
+-- Boston Crab Meat, Rd Kaviar, Sasquatch Ale, Rhnbru Klosterbier, Geitost.
 -- Order them in descending order by price.
 -- If two or more have the same price, list those in ascending order by product name.
+SELECT ProductName, UnitsInStock, UnitPrice
+FROM products
+WHERE UnitsInStock >= 100
+ORDER BY UnitPrice DESC, ProductName ASC;
+
+-- 4) Write a query against the orders table that displays the total number of distinct
+-- customers who have placed orders, based on customer ID. Use an alias to label the
+-- count calculation as CustomerCount.
