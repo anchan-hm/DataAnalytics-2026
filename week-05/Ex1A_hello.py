78a49c84e9fab9b0374c99d8f89400423dc05240
Alexus Chanthadara <alexus.n.chanthadara@gmail.com>
2026-05-04 14:21:20 -0400

    adding 1A vscode

--- week-05/Ex1A_hello.py
+++ week-05/Ex1A_hello.py
@@ -1,1 +1,20 @@
 print("Hello World!")
+
+message = 'Hello World!'
+print(message)
+# "Hello World" prints twice because becaause I have two different print statements
+
+# displaying dollars and cents
+dollars = 3
+cents = .50
+print(dollars + cents)
+# I have made dollars as 3 and cents as .50, adding them together created 3.50 instead of "dollarscents"
+
+cents = cents + .25
+print(dollars + cents)
+# new results, 3.75
+
+d_str = '3 dollars'
+c_str = '50 cents'
+print(d_str + " " + c_str)
+# output, 3 dollars 50 cents
