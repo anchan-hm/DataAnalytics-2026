7b0dc3424e2cc5d3b37ae24cd727685b3f30b8da
Alexus Chanthadara <alexus.n.chanthadara@gmail.com>
2026-05-05 13:43:19 -0400

    adding f-string

--- week-05/Ex2A_MathScripts/area_of_circle.py
+++ week-05/Ex2A_MathScripts/area_of_circle.py
@@ -9,6 +9,6 @@ radius = diameter/2 # radius is half of a diameter
 # calculating the area
 area = math.pi*(radius**2)
 
-# displaying the results
-print("The area of a circle with radius " + str(radius) + " is " + str(area))
+# displaying the results with f-string
+print(f"The area of the circle with radius {radius} is {area:.2f}")
 # The area of a circle with radius 10.0 is 314.1592653589793
