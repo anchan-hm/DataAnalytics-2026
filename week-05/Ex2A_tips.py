7bf6cd40eec300a4fd1044c9822dd37a7cb3bbaf
Alexus Chanthadara <alexus.n.chanthadara@gmail.com>
2026-05-04 22:02:19 -0400

    adding Ex2A

---
+++ week-05/Ex2A_tips.py
@@ -0,0 +1,23 @@
+# **Lab 1**
+# Sample problem:  How do you calculate the total due at a restaurant given the food cost,
+# tax, and tip?
+
+# Define known values
+food_cost = 79.25
+tax = 6.54
+tip = 12.00
+
+# Calculate the unknown
+total_due = food_cost + tax + tip
+
+# Display the results
+# print("The total due is " + str(total_due)) # total due = 97.79
+# a) Remember what the str() function is for? Why is this being used here? Add your
+# answer as a comment.
+# the str() is used to turn integers into text and be able to be part of a message
+
+# practice complicated output for print statement
+print("Food cost is " + str(food_cost) + " and tax is " + str(tax))
+# print("Tip is " + str(tip))
+print("Tip is " + format(tip, ".2f"))
+print("Total due is " + str(total_due))
