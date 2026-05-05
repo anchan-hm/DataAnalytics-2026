628cb25d5768fd85fff0c8a1e0fdd44e72913c2d
Alexus Chanthadara <alexus.n.chanthadara@gmail.com>
2026-05-05 13:35:30 -0400

    updating for input()

--- week-05/Ex2A_MathScripts/tip_amount.py
+++ week-05/Ex2A_MathScripts/tip_amount.py
@@ -1,12 +1,22 @@
-# calculating tip amount on restaurant bill given the tip percentage
+# calculating tip amount on restaurant bill given the tip percentage (Updated for Lab 3)
 
 # variables
-bill = 38.50
-tip_percentage = 0.20 # 20% tip 
+bill = float(input("How much is the restaurant bill? "))
+tip_percentage = float(input("What percent tip do you want to leave? ")) 
+
+# tip percentage to decimal
+tip_deci = tip_percentage/100
 
 # calculating the tip amount
-tip_amount = bill * tip_percentage
+tip_amount = bill * tip_deci
 
 # displaying the results
-print("The tip on a $"+str(bill) + " restaurant bill is $"+str(tip_amount))
-# The tip on a $38.5 restaurant bill is $7.7
+print("The tip on a $"+str(bill) + " restaurant bill is $"+format(tip_amount, ".2f"))
+# How much is the restaurant bill? 34.50
+# What percent tip do you want to leave? 20
+# The tip on a $34.5 restaurant bill is $6.9
+
+# pitfalls with input()
+# - have to turn it into a float for any math
+# - typing letters instead of numbers or else it'll crash
+# - not putting anything will cause it to crash
