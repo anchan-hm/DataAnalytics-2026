dbd999ceac842a64ecd831503b68d30d854f9317
Alexus Chanthadara <alexus.n.chanthadara@gmail.com>
2026-05-05 10:25:15 -0400

    adding area of rectangle

---
+++ week-05/Ex2A_MathScripts/area_of_rectangle.py
@@ -0,0 +1,13 @@
+# how do you calculate the area of a rectangle?
+
+# rectangle dimensions using birthday
+side_a = 4 #month
+side_b = 20 #day
+
+# calculating the area
+area = side_a * side_b
+
+# displaying the results
+print("Side A is " + str(side_a)) # 4
+print("Side B is " + str(side_b)) # 20
+print("The area of the rectangle is " + str(area)) # 80
