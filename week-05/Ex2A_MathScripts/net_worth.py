f339e9c94127175c2fef79f57b630453d075552f
Alexus Chanthadara <alexus.n.chanthadara@gmail.com>
2026-05-06 08:20:12 -0400

    updating results for net worth

--- week-05/Ex2A_MathScripts/net_worth.py
+++ week-05/Ex2A_MathScripts/net_worth.py
@@ -26,6 +26,9 @@ total_debts = student_loans + credit_cards + personal_loans #calculating debts
 net_worth = total_assets - total_debts
 
 # displaying outputs
-print("Your total assets are " + str(total_assets)) # 213000
-print("Your total debts are " + str(total_debts)) # 3830
-print("Your net worth is " + str(net_worth)) # 209170
+print("Your total assets are " + str(total_assets)) 
+print("Your total debts are " + str(total_debts)) 
+print("Your net worth is " + str(net_worth))
+# Your total assets are 213000
+# Your total debts are 3830
+# Your net worth is 209170
