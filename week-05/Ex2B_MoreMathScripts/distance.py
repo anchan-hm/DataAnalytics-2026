5944428a7ccd835870c38d5c6a57f25f7961d962
Alexus Chanthadara <alexus.n.chanthadara@gmail.com>
2026-05-06 08:28:44 -0400

    using square root

---
+++ week-05/Ex2B_MoreMathScripts/distance.py
@@ -0,0 +1,17 @@
+# calculating distance
+
+# importing library
+import math
+
+# coordinates
+x1 = 2
+y1 = 3
+x2= 4
+y2 = 6
+
+# square root formula
+distance = math.sqrt((x2-x1)**2+(y2-y1)**2)
+
+# displaying result
+print(f"The distance between points is {distance:.2f}")
+# The distance between points is 3.61
