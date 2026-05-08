6d518c177ad8b61684da23a355af04d09ba119e8
Alexus Chanthadara <alexus.n.chanthadara@gmail.com>
2026-05-08 08:20:44 -0400

    love for loops

---
+++ week-05/Ex4C_LoopScripts/loop_love.py
@@ -0,0 +1,17 @@
+# creating a loop for love
+
+# list for words the mean "great"
+great = ["amazing", "awesome", "excellent", "outstanding", "steller"]
+
+# using "for" to loop through list
+for word in great:
+    print("Loops are", word + "!")
+
+# final print when loop is finished
+print("I <3 Loops")
+#output: Loops are amazing!
+#        Loops are awesome!
+#        Loops are excellent!
+#        Loops are outstanding!
+#        Loops are steller!
+#        I <3 Loops
