333093d60e716366e7efd016c820ab1582214ca0
Alexus Chanthadara <alexus.n.chanthadara@gmail.com>
2026-05-14 15:44:46 -0400

    adding write file

---
+++ week-06/Ex4A_ReadWriteFiles/open_and_write.py
@@ -0,0 +1,15 @@
+# creating variable to use open function
+
+# creating about_me.txt to replace
+f = open("about_me.txt", "a") # adding file if missing
+
+# adding perfect night out
+f.write("\nIf I could have the perfect night out, I would go to my favorite boba and east some onigiri.\n")
+
+f.close() # closing file once created
+#output: Name: Alexus
+#        Place of birth: North Carolina
+#        Pets growing up: dogs
+#        Travel to for ONE WEEK: Greece
+#        Live at for a YEAR: Japan
+#        If I could have the perfect night out, I would go to my favorite boba and east some onigiri.
