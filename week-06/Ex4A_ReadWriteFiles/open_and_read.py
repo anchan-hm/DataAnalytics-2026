47b058df9e0b01936cda96e4660f6fde2105af51
Alexus Chanthadara <alexus.n.chanthadara@gmail.com>
2026-05-14 21:59:07 -0400

    adding read, readline, readlines method

--- week-06/Ex4A_ReadWriteFiles/open_and_read.py
+++ week-06/Ex4A_ReadWriteFiles/open_and_read.py
@@ -21,11 +21,11 @@ f = open("about_me.txt", "r")
 #        Liv
 
 # printing even more parts
-print(f.readline(10)) # first 10 of first line
-print(f.readline()) # reads the rest of that line
-
-for i in range(1,5): #looping
-    print(f.readline()) # reading the next 4 lines
+# print(f.readline(10)) # first 10 of first line
+# print(f.readline()) # reads the rest of that line
+#
+# for i in range(1,5): #looping
+#     print(f.readline()) # reading the next 4 lines
 #output: Name: Alex
 #        us
 #
@@ -36,6 +36,62 @@ for i in range(1,5): #looping
 #        Travel to for ONE WEEK: Greece
 #
 #        Live at for a YEAR: Japan
+
+# using readlines()
+# a) print(f.readlines(1)) # reading parts of the first line
+#output: ['Name: Alexus\n']
+
+# b) print(f.readlines(1)) # continuing
+#output: ['Name: Alexus\n']
+#        ['Place of birth: North Carolina\n']
+
+# c) print(f.readlines(10)) # reading more
+#output: ['Name: Alexus\n']
+#        ['Place of birth: North Carolina\n']
+#        ['Pets growing up: dogs\n']
+
+# d) print(f.readlines(10)) # continues reading
+#output: ['Name: Alexus\n']
+#        ['Place of birth: North Carolina\n']
+#        ['Pets growing up: dogs\n']
+#        ['Travel to for ONE WEEK: Greece\n']
+
+# e1) print(f.readlines(100)) # reading a larger amount
+#output: ['Name: Alexus\n']
+#        ['Place of birth: North Carolina\n']
+#        ['Pets growing up: dogs\n']
+#        ['Travel to for ONE WEEK: Greece\n']
+#        ['Live at for a YEAR: Japan\n', 'If I could have the perfect night out, I would go to my favorite boba and eat some onigiri.\n']
+
+# e2) print(f.readlines(-1)) # finish reading the file
+#output: ['Name: Alexus\n']
+#        ['Place of birth: North Carolina\n']
+#        ['Pets growing up: dogs\n']
+#        ['Travel to for ONE WEEK: Greece\n']
+#        ['Live at for a YEAR: Japan\n', 'If I could have the perfect night out, I would go to my favorite boba and eat some onigiri.\n']
+#        []
+
+# trying a new combination of read methods
+# a) first 50 characters
+first_50 = f.read(50)
+
+# b) next 4 lines being stored as list
+next_lines = []
+for i in range(4):
+    next_lines.append(f.readline())
+
+# c) read next 100 characters
+next_100 = f.readlines(100)
+
+# displaying output
+print("First 50 characters:", first_50)
+print("Next 4 lines, as list:", next_lines)
+print("Next 100 characters:", next_100)
+#output: First 50 characters: Name: Alexus
+#        Place of birth: North Carolina
+#        Pets g
+#        Next 4 lines, as list: ['rowing up: dogs\n', 'Travel to for ONE WEEK: Greece\n', 'Live at for a YEAR: Japan\n', 'If I could have the perfect night out, I would go to my favorite boba and eat some onigiri.\n']
+#        Next 100 characters: []
 
 # close file
 f.close()
